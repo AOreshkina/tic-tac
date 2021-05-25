e899478491a48da2471d712a0ad1412d3378bea1
AOreshkina <nastyorekhova@yandex.ru>
2021-05-25 17:03:44 +0300

    Button deactivation delay

--- tictac.js
+++ tictac.js
@@ -1,18 +1,34 @@
 
  function ready() {
 
-  /*(".choose-human .buttons").click(function(){ // задаем функцию при нажатиии на элемент <button>
-    ; // вызываем событие click на элементе <div>
-  });*/
+  var player_robot = document.getElementsByClassName("choose-robot");
+  var player_human = document.getElementsByClassName("choose-human");
+  let choise;
+  let first_player;
+  let second_player;
 
     
-  function displayDate() {
+  function choiseRobot() {
     document.getElementById("humanjpg").setAttribute("src", ".\\img\\you.jpg");
+    //document.getElementsById("button_ii").setAttribute("id", "deactive1");
+   // document.getElementsById("button_human").setAttribute("id", "deactive2");
+
+    choise = "Robot";
+  }
+  function choiseHuman() {
+    document.getElementById("robotjpg").setAttribute("src", ".\\img\\you.jpg");
+    choise = "human";
   }
 
-  document.getElementById("button_ii").onclick = displayDate;
+  document.getElementById("button_ii").onclick = choiseRobot;
+  document.getElementById("button_human").onclick = choiseHuman;
+
+  if (choise==="robot"){
+    console.log('Here');
+    player_human.getElementsByClass("buttons").setAttribute("id", " ");
+  }
  
- document.getElementsByClassName('choose-robot').click = function(){alert("ROBOT")};
+// document.getElementsByClassName('choose-robot').click = function(){alert("ROBOT")};
  // let imageRobot = chooseRobot.getElementsByTagName('img');
   //let buttonHuman =chooseHuman.getElementsByClassName('buttons');
  // buttonHuman.click()
