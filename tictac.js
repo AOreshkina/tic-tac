02f4c9d592cc7531236179a3e6995aba622e94f3
AOreshkina <nastyorekhova@yandex.ru>
2021-06-04 01:38:03 +0300

    Add new image. Add victory tecst

--- tictac.js
+++ tictac.js
@@ -10,18 +10,10 @@
   the_buttons = document.getElementsByClassName("buttons");
   let player_robot = document.getElementById("button_ii");
   let player_human = document.getElementById("button_human");
+  let your_symbol = document.getElementById("victoria");
+  let tabels = document.getElementsByClassName("void");
+ // let view_table_element = document.getElementsByClassName("void");
 
-
-/*function disabledButtons(){
-  if (choise==="Robot"){
-  the_buttons[1].setAttribute("class","disabled");
-  }
-  if(choise==="Human"){
-    the_buttons[0].setAttribute("class","disabled");
-   }
-}*/
-    
-  
 
   the_buttons[0].onclick = choiseRobot;
   the_buttons[1].onclick = choiseHuman;
@@ -30,32 +22,38 @@
     
     document.getElementById("humanjpg").setAttribute("src", ".\\img\\you.jpg");
     player_human.disabled = true; 
-   // player_human.classList.add("disabled");
+    your_symbol.innerHTML=`<div id="victory_text">Ты играешь ноликами</div>`;//<img src=".\\img\\yourchoice2.jpg" alt="" id="symbol_zero">
     choise = 'robot';
     console.log(choise);
-   // choise = "Robot";  
   }
+  
 
   function choiseHuman() {
     document.getElementById("robotjpg").setAttribute("src", ".\\img\\you.jpg");
     player_robot.disabled = true; 
-   // player_robot.classList.add("disabled");
+    your_symbol.innerHTML=`<div id="victory_text">Ты играешь крестиками</div>`;
     choise = 'human';
     console.log(choise);
+    
   }
- 
- //chooseHuman.getElementsByClassName('buttons').onclick = function(){alert("ROBOT")};
-  
- 
-// document.getElementsByClassName('choose-robot').click = function(){alert("ROBOT")};
- // let imageRobot = chooseRobot.getElementsByTagName('img');
-  //let buttonHuman =chooseHuman.getElementsByClassName('buttons').onclick = unction(){alert("ROBOT")};
- // buttonHuman.click()
- //   alert('Cgfcb,kkk!');
- //  imageRobot.setAttribute(`class`, `=.\img\you.jpg`);
- // }
 
- // alert("HELLO!");*
+  
+
+  tabels.forEach(function(item){
+    if(choise==='human'){
+      item.onclick=item.setAttribute("src",".\\img\\cross1.jpg")
+
+    }
+    
+  })
+
+
+ 
+  /*if (choise=='robot'){
+    console.log("jfgkgjfgfkg");
+    
+    your_symbol.innerHTML=`<img src=".\img\yourchoice2.jpg" alt="" id="symbol_zero">`;
+  }*/
  
 }
  
