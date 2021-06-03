6ff4843de7cd26a1b721570cc6f13333587f7339
AOreshkina <nastyorekhova@yandex.ru>
2021-06-03 21:59:27 +0300

    Lock the selection buttons

--- tictac.js
+++ tictac.js
@@ -1,38 +1,55 @@
 
-  let player_robot = document.getElementsByClassName("choose-robot");
-  let player_human = document.getElementsByClassName("choose-human");
+  let the_buttons =  [];
+  
   let choise;
   let first_player;
   let second_player;
 
  function ready() {
 
+  the_buttons = document.getElementsByClassName("buttons");
+  let player_robot = document.getElementById("button_ii");
+  let player_human = document.getElementById("button_human");
 
-function disabledButtons(){
+
+/*function disabledButtons(){
   if (choise==="Robot"){
-   // console.log('Here');
-   player_human.getElementsById("button_human").setAttribute("id","34");
+  the_buttons[1].setAttribute("class","disabled");
   }
-}
+  if(choise==="Human"){
+    the_buttons[0].setAttribute("class","disabled");
+   }
+}*/
     
+  
+
+  the_buttons[0].onclick = choiseRobot;
+  the_buttons[1].onclick = choiseHuman;
+
   function choiseRobot() {
+    
     document.getElementById("humanjpg").setAttribute("src", ".\\img\\you.jpg");
-    choise = "Robot";
-    disabledButtons();
+    player_human.disabled = true; 
+   // player_human.classList.add("disabled");
+    choise = 'robot';
+    console.log(choise);
+   // choise = "Robot";  
   }
+
   function choiseHuman() {
     document.getElementById("robotjpg").setAttribute("src", ".\\img\\you.jpg");
-    choise = "human";
+    player_robot.disabled = true; 
+   // player_robot.classList.add("disabled");
+    choise = 'human';
+    console.log(choise);
   }
-
-  document.getElementById("button_ii").onclick = choiseRobot;
-  document.getElementById("button_human").onclick = choiseHuman;
-
+ 
+ //chooseHuman.getElementsByClassName('buttons').onclick = function(){alert("ROBOT")};
   
  
 // document.getElementsByClassName('choose-robot').click = function(){alert("ROBOT")};
  // let imageRobot = chooseRobot.getElementsByTagName('img');
-  //let buttonHuman =chooseHuman.getElementsByClassName('buttons');
+  //let buttonHuman =chooseHuman.getElementsByClassName('buttons').onclick = unction(){alert("ROBOT")};
  // buttonHuman.click()
  //   alert('Cgfcb,kkk!');
  //  imageRobot.setAttribute(`class`, `=.\img\you.jpg`);
