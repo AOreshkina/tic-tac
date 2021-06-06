bc653327cc3b5357cda9841f01ded2df70ec8f71
AOreshkina <nastyorekhova@yandex.ru>
2021-06-07 01:28:21 +0300

    Change names after selecting an opponent

--- tictac.js
+++ tictac.js
@@ -12,7 +12,8 @@
   let player_robot = document.getElementById("button_ii");
   let player_human = document.getElementById("button_human");
   let your_symbol = document.getElementById("victoria");
-  let tabels = [];
+  //let tabels = [];
+  let opponent = document.getElementById("choose_opponent");
   tabels= document.getElementsByClassName("void");
  // let view_table_element = document.getElementsByClassName("void");
 
@@ -24,6 +25,8 @@
     
     document.getElementById("humanjpg").setAttribute("src", ".\\img\\you.jpg");
     player_human.disabled = true; 
+    opponent.innerText=`Статус игры`;
+    the_buttons[1].innerText=`Ты`;
     your_symbol.innerHTML=`<div id="victory_text">Ты играешь ноликами</div>`;//<img src=".\\img\\yourchoice2.jpg" alt="" id="symbol_zero">
     choise = 'robot';
     console.log(choise);
@@ -33,6 +36,8 @@
   function choiseHuman() {
     document.getElementById("robotjpg").setAttribute("src", ".\\img\\you.jpg");
     player_robot.disabled = true; 
+    opponent.innerText=`Статус игры`;
+    the_buttons[0].innerText=`Ты`;
     your_symbol.innerHTML=`<div id="victory_text">Ты играешь крестиками</div>`;
     choise = 'human';
     console.log(choise);
@@ -81,159 +86,6 @@
   
 
 
- /* document.getElementById("a1").onclick = function(){
-    if(choise==='human'){
-      if(count%2==0){
-        document.getElementById("a1").innerHTML=`<img src=".\\img\\cross1.jpg" alt="" class="void">`;
-        document.getElementById("a1").setAttribute("id", "a10");
-        count=count+1;
-        tabel_elem[0][0]='iscross';
-        checkingTheWinner();
-      }else{
-        document.getElementById("a1").innerHTML=`<img src=".\\img\\zero1.jpg" alt="" class="void">`;
-        document.getElementById("a1").setAttribute("id", "a10");
-        count=count+1;
-        tabel_elem[0][0]='isnull';
-        checkingTheWinner();
-
-      }}}
-    
-           
-  document.getElementById("a2").onclick = function(){
-    if(choise==='human'){
-      if(count%2==0){
-        document.getElementById("a2").innerHTML=`<img src=".\\img\\cross1.jpg" alt="" class="void">`;
-        document.getElementById("a2").setAttribute("id", "a20");
-        count=count+1;
-        tabel_elem[0][1]='iscross';
-        checkingTheWinner();
-      }else{
-        document.getElementById("a2").innerHTML=`<img src=".\\img\\zero1.jpg" alt="" class="void">`;
-        document.getElementById("a2").setAttribute("id", "a20");
-        count=count+1;
-        tabel_elem[0][1]='isnull';
-        checkingTheWinner();
-
-      }}}
-      
-  document.getElementById("a3").onclick = function(){
-    if(choise==='human'){
-      if(count%2==0){
-        document.getElementById("a3").innerHTML=`<img src=".\\img\\cross1.jpg" alt="" class="void">`;
-        document.getElementById("a3").setAttribute("id", "a30");
-        count=count+1;
-        tabel_elem[0][2]='iscross';
-        checkingTheWinner();
-      }else{
-        document.getElementById("a3").innerHTML=`<img src=".\\img\\zero1.jpg" alt="" class="void">`;
-        document.getElementById("a3").setAttribute("id", "a30");
-        count=count+1;
-        tabel_elem[0][2]='isnull';
-        checkingTheWinner();
-
-      }}}
-
-  document.getElementById("b1").onclick = function(){
-    if(choise==='human'){
-      if(count%2==0){
-        document.getElementById("b1").innerHTML=`<img src=".\\img\\cross1.jpg" alt="" class="void">`;
-        document.getElementById("b1").setAttribute("id", "b10");
-        count=count+1;
-        tabel_elem[1][0]='iscross';
-        checkingTheWinner();
-      }else{
-        document.getElementById("b1").innerHTML=`<img src=".\\img\\zero1.jpg" alt="" class="void">`;
-        document.getElementById("b1").setAttribute("id", "b10");
-        count=count+1;
-        tabel_elem[1][0]='isnull';
-        checkingTheWinner();
-
-      }}}
-    
-           
-  document.getElementById("b2").onclick = function(){
-    if(choise==='human'){
-      if(count%2==0){
-        document.getElementById("b2").innerHTML=`<img src=".\\img\\cross1.jpg" alt="" class="void">`;
-        document.getElementById("b2").setAttribute("id", "b20");
-        count=count+1;
-        tabel_elem[1][1]='iscross';
-        checkingTheWinner();
-      }else{
-        document.getElementById("b2").innerHTML=`<img src=".\\img\\zero1.jpg" alt="" class="void">`;
-        document.getElementById("b2").setAttribute("id", "b20");
-        count=count+1;
-        tabel_elem[1][1]='isnull';
-        checkingTheWinner();
-
-      }}}
-      
-  document.getElementById("b3").onclick = function(){
-    if(choise==='human'){
-      if(count%2==0){
-        document.getElementById("b3").innerHTML=`<img src=".\\img\\cross1.jpg" alt="" class="void">`;
-        document.getElementById("b3").setAttribute("id", "b30");
-        count=count+1;
-        tabel_elem[1][2]='iscross';
-        checkingTheWinner();
-      }else{
-        document.getElementById("b3").innerHTML=`<img src=".\\img\\zero1.jpg" alt="" class="void">`;
-        document.getElementById("b3").setAttribute("id", "b30");
-        count=count+1;
-        tabel_elem[1][2]='isnull';
-        checkingTheWinner();
-      }}}
-
-document.getElementById("c1").onclick = function(){
-    if(choise==='human'){
-      if(count%2==0){
-        document.getElementById("c1").innerHTML=`<img src=".\\img\\cross1.jpg" alt="" class="void">`;
-        document.getElementById("c1").setAttribute("id", "c10");
-        count=count+1;
-        tabel_elem[2][0]='iscross';
-        checkingTheWinner();
-      }else{
-        document.getElementById("c1").innerHTML=`<img src=".\\img\\zero1.jpg" alt="" class="void">`;
-        document.getElementById("c1").setAttribute("id", "c10");
-        count=count+1;
-        tabel_elem[2][0]='isnull';
-        checkingTheWinner();
-
-      }}}
-    
-           
-  document.getElementById("c2").onclick = function(){
-    if(choise==='human'){
-      if(count%2==0){
-        document.getElementById("c2").innerHTML=`<img src=".\\img\\cross1.jpg" alt="" class="void">`;
-        document.getElementById("c2").setAttribute("id", "c20");
-        count=count+1;
-        tabel_elem[2][1]='iscross';
-        checkingTheWinner();
-      }else{
-        document.getElementById("c2").innerHTML=`<img src=".\\img\\zero1.jpg" alt="" class="void">`;
-        document.getElementById("c2").setAttribute("id", "c20");
-        count=count+1;
-        tabel_elem[2][1]='isnull';
-        checkingTheWinner();
-
-      }}}
-      
-  document.getElementById("c3").onclick = function(){
-    if(choise==='human'){
-      if(count%2==0){
-        document.getElementById("c3").innerHTML=`<img src=".\\img\\cross1.jpg" alt="" class="void">`;
-        document.getElementById("c3").setAttribute("id", "c30");
-        count=count+1;
-        tabel_elem[2][2]='iscross';
-        checkingTheWinner();
-      }else{
-        document.getElementById("c3").innerHTML=`<img src=".\\img\\zero1.jpg" alt="" class="void">`;
-        document.getElementById("c3").setAttribute("id", "c30");
-        count=count+1;
-        tabel_elem[2][2]='isnull';
-        checkingTheWinner();
-
-      }}}*/
+ 
  
           }ready();
