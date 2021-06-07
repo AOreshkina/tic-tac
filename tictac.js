9b8fe9577be57a2e3f3fc9e351aabb03e99c8fbd
AOreshkina <nastyorekhova@yandex.ru>
2021-06-08 01:59:33 +0300

    Add logic for choosing winners among people

--- tictac.js
+++ tictac.js
@@ -17,7 +17,7 @@
   tabels= document.getElementsByClassName("void");
  // let view_table_element = document.getElementsByClassName("void");
 
-
+  let winners_arr = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   the_buttons[0].onclick = choiseRobot;
   the_buttons[1].onclick = choiseHuman;
 
@@ -25,6 +25,7 @@
     
     document.getElementById("humanjpg").setAttribute("src", ".\\img\\you.jpg");
     player_human.disabled = true; 
+    player_robot.disabled = true; 
     opponent.innerText=`Статус игры`;
     the_buttons[1].innerText=`Ты`;
     your_symbol.innerHTML=`<div id="victory_text">Ты играешь ноликами</div>`;//<img src=".\\img\\yourchoice2.jpg" alt="" id="symbol_zero">
@@ -36,6 +37,7 @@
   function choiseHuman() {
     document.getElementById("robotjpg").setAttribute("src", ".\\img\\you.jpg");
     player_robot.disabled = true; 
+    player_human.disabled = true;
     opponent.innerText=`Статус игры`;
     the_buttons[0].innerText=`Ты`;
     your_symbol.innerHTML=`<div id="victory_text">Ты играешь крестиками</div>`;
@@ -44,19 +46,27 @@
     
   }
 
-  let tabel_elem =[ ['n','n','n'],
-                    ['n','n','n'],
-                    ['n','n','n']
-                  ];
+  
 
-  function checkingTheWinner(){
-    if((tabel_elem[0][0]==tabel_elem[0][1]==tabel_elem[0][2])||(tabel_elem[1][0]==tabel_elem[1][1]==tabel_elem[1][2])||(tabel_elem[2][0]==tabel_elem[2][1]==tabel_elem[2][2])||(tabel_elem[0][0]==tabel_elem[1][0]==tabel_elem[2][0])||(tabel_elem[0][1]==tabel_elem[1][1]==tabel_elem[2][1])||(tabel_elem[0][2]==tabel_elem[1][2]==tabel_elem[2][2])||(tabel_elem[0][0]==tabel_elem[1][1]==tabel_elem[2][2])||(tabel_elem[0][2]==tabel_elem[1][1]==tabel_elem[2][0])){
-      if(tabel_elem[0][0]=='iscross'){
-       console.log('rtrtrtrtrt');
-      }
+  function checkingTheWinnerForHuman(){
+    let row_1 = winners_arr[0]+winners_arr[1]+winners_arr[2];
+    let row_2 = winners_arr[3]+winners_arr[4]+winners_arr[5];
+    let row_3 = winners_arr[6]+winners_arr[7]+winners_arr[8];
+    let column_1 = winners_arr[0]+winners_arr[3]+winners_arr[6];
+    let column_2 = winners_arr[1]+winners_arr[4]+winners_arr[7];
+    let column_3 = winners_arr[2]+winners_arr[5]+winners_arr[8];
+    let diagonal_1 = winners_arr[0]+winners_arr[4]+winners_arr[8];
+    let diagonal_2 = winners_arr[2]+winners_arr[4]+winners_arr[6];
+    if ((row_1===6)||(row_2===6)||(row_3===6)||(column_1===6)||(column_2===6)||(column_3===6)||(diagonal_1===6)||(diagonal_2===6)){
+      your_symbol.innerHTML=`<div id="victory_text">Ты выиграл!</div>`;
+    } else if ((row_1==27)||(row_2==27)||(row_3==27)||(column_1==27)||(column_2==27)||(column_3==27)||(diagonal_1==27)||(diagonal_2==27)){
+      your_symbol.innerHTML=`<div id="victory_text">Выиграл твой друг!</div>`;
+    } else {
+      your_symbol.innerHTML=`<div id="victory_text">Ничья!</div>`;
     }
 
-  }
+      }
+    
 
  
   let table = document.getElementById("table_c");
@@ -71,17 +81,27 @@
     if(choise==='human'){
       if(count%2==0){
        target.innerHTML=`<img src=".\\img\\cross1.jpg" alt="" class="void">`;
+       let target_id = target.getAttribute("id");
+       let num_array = Number(target_id) ;
+       winners_arr[num_array] = 2;
+       console.log(winners_arr);
        target.disabled = true;
         count=count+1;
+        checkingTheWinnerForHuman();
         //tabel_elem[0][0]='iscross';
         //checkingTheWinner();
       }else{
         target.innerHTML=`<img src=".\\img\\zero1.jpg" alt="" class="void">`;
         target.setAttribute("contenteditable","false");
+        let target_id = target.getAttribute("id");
+       let num_array = Number(target_id) ;
+       winners_arr[num_array] = 9;
+       console.log(winners_arr);
         count=count+1;
         target.disabled = true;
      //  tabel_elem[0][0]='isnull';
        // checkingTheWinner();
+       checkingTheWinnerForHuman();
 
       }
 
